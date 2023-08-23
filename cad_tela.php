763a5eeab4b54a5e067b599ba7a2a49817046388
Gerbercode <139247388+Gerbercode@users.noreply.github.com>
2023-08-23 20:35:36 +0100

    Add files via upload

---
+++ cad_tela.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="UTF-8">
+<title>Cadastro concluído</title>
+<style>
+
+h2{
+font-align:center;
+font-size:19px;
+color:blue;
+    
+}
+
+h1{
+
+font-size:40px;
+color:white;
+    
+}
+
+p{
+color: white;    
+}
+
+button{
+font-size:28px;
+border-radius: 15px;
+border-color:blue; 
+color:blue; 
+margin:50px;  
+} 
+     
+ </style>   
+   
+</head>
+<body  background =img/robot.jpg>
+    
+<hr>
+<br>
+<h2> SEUS DADOS FORAM SALVOS COM SUCESSO!</h2>
+<hr>
+<p> Esta página ainda não possui contéudo!</p>
+<hr>
+
+<h1><center>• MY WEBSITE • </center></h1>
+<hr>
+<a href="index.html"><button> Ir para página principal</button></a>
+
+    
+</body>
+</html>
---
+++ d_login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Bem Vindo</title>
+<style>
+
+h2{
+
+text-align:center;
+font-size: 30px;
+color:white;
+
+   
+}
+
+h1{
+
+text-align:center;
+font-size: 30px;
+color:blue;
+
+   
+}
+
+p{
+color: white;    
+}
+
+
+button{
+margin: 50px; 
+color: blue;
+border-radius: 10px;
+font-size:25px;
+border-color:blue; 
+ }
+
+
+   
+    
+</style>    
+    
+    
+</head>
+<body background= img/grut.jpg >
+  
+
+<hr>
+<br>
+<h1> < Olá caro usuário 🖐🖐🖐 > </h1>
+
+<h2> < BEM VINDO DE VOLTA > </h2>
+<hr>
+<p> Esta página ainda não possui contéudo!</p>
+
+
+<hr>
+<h1> • MY WEBSITE • </h1>
+<hr>
+<a href="index.html"><button> Ir para página principal </button></a>
+
+ 
+ 
+ 
+ 
+    
+  
+</body>
+</html>

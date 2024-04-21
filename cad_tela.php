3f6f6efa76ac224d33cedcc83d6998dfae301de0
Gerber Neves <139247388+Gerbercode@users.noreply.github.com>
2024-04-21 14:08:31 +0100

    comment

--- cad_tela.php
+++ cad_tela.php
@@ -5,24 +5,25 @@
 <title>Cadastro concluído</title>
 <style>
 
+<!-- Centers the h2 element and sets its font size to 19px, color to blue -->
 h2{
 font-align:center;
 font-size:19px;
 color:blue;
-    
 }
 
+<!-- Centers the h1 element, sets its font size to 40px, and color to white -->
 h1{
-
 font-size:40px;
 color:white;
-    
 }
 
+<!-- Sets the color of p elements to white -->
 p{
 color: white;    
 }
 
+<!-- Styles the button element with a border radius of 15px, border color of blue, button color of blue, margin of 50px, and font size of 28px -->
 button{
 font-size:28px;
 border-radius: 15px;
@@ -31,22 +32,21 @@ color:blue;
 margin:50px;  
 } 
      
- </style>   
+</style>   
    
 </head>
-<body  background =img/robot.jpg>
+<body  background ="img/robot.jpg"> <!-- Sets the background image to "img/robot.jpg" -->
     
-<hr>
-<br>
-<h2> SEUS DADOS FORAM SALVOS COM SUCESSO!</h2>
-<hr>
-<p> Esta página ainda não possui contéudo!</p>
-<hr>
+<hr> <!-- Adds a horizontal rule -->
+<br> <!-- Adds a line break -->
+<h2> SEUS DADOS FORAM SALVOS COM SUCESSO!</h2> <!-- Displays a success message -->
+<hr> <!-- Adds a horizontal rule -->
+<p> Esta página ainda não possui contéudo!</p> <!-- Displays a message indicating that the page is empty -->
+<hr> <!-- Adds a horizontal rule -->
 
-<h1><center>• MY WEBSITE • </center></h1>
-<hr>
-<a href="index.html"><button> Ir para página principal</button></a>
+<h1><center>• MY WEBSITE • </center></h1> <!-- Displays the website title -->
+<hr> <!-- Adds a horizontal rule -->
+<a href="index.html"><button> Ir para página principal</button></a> <!-- Creates a link to the homepage -->
 
-    
 </body>
 </html>
